5255895cdca036c380ca324bd45429a09cfa9006
Vladimir Stupin <vladimir@stupin.su>
2021-08-01 02:50:08 +0500

    В скрипт сборки добавлены опции для удаления из исполняемого файла неиспользуемых функций и данных

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gcc -std=c99 -Wpedantic -Wall -Wextra -D_POSIX_C_SOURCE=200809L -o usr types.c proto.c net.c actions.c main.c
+gcc -std=c99 -Wpedantic -Wall -Wextra -D_POSIX_C_SOURCE=200809L -fdata-sections -ffunction-sections -Wl,--gc-sections -Wl,--print-gc-sections -Wl,-s -o usr types.c proto.c net.c actions.c main.c
